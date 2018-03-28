38416475048488125fad4d8e3c291f264f29a578
Gebreyesus <beteab@live.com>
2018-03-28 00:47:38 -0500

    Add project files.

---
+++ CodingGround-Proj2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodingGround_Proj2.MvcApplication" Language="C#" %>
---
+++ CodingGround-Proj2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodingGround_Proj2.Startup))]
+namespace CodingGround_Proj2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
